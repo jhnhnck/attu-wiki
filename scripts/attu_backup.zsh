fb29d3e164e7d1b322defe1e229777082381026b
John Hancock <jhnhnck@proton.me>
2025-08-12 23:56:19 -0400

    trim this down a bit for now

--- scripts/attu_backup.zsh
+++ scripts/attu_backup.zsh
@@ -1,21 +1,15 @@
 #!/usr/bin/env zsh
 
-# Import Secrets
+# I can't seem to stop systend from running this at boot
+boot_secs=$(printf '%.0f\n' "$(cut -d' ' -f1 </proc/uptime)")
+if [ "$boot_secs" -lt 300 ]; then
+    printf '%s\n' "Script caught running ${boot_secs}s after boot; exiting"
+    exit 0
+fi
+
 SCRIPT_SOURCE=${0%/*}
 cd $SCRIPT_SOURCE/..
 source .env
-
-# Configuration
-backup_path='/srv/backups/attu-wiki'
-bot_backup_path='/srv/backups/attu-bot'
-heartbeat_url="https://uptime.betterstack.com/api/v1/heartbeat/${BACKUPS_HEARTBEAT_KEY}"
-min_backup_size=10000
-wiki_path="$PWD"
-mw_container='mediawiki'
-db_container='database'
-
-# Exit Trap
-doombot_db_path='/srv/services/doom-bot/assets/markers.db'
 
 exit_trap() {
     printf '%s\n' "caught error; sending fail hook"
@@ -24,106 +18,73 @@ exit_trap() {
 }
 
 trap 'exit_trap' ZERR
+set -eu
 
-boot_secs=$(printf '%.0f\n' "$(cut -d' ' -f1 </proc/uptime)")
-if [ "$boot_secs" -lt 300 ]; then
-    printf '%s\n' "Script caught running ${boot_secs}s after boot; exiting"
-    exit 0
-fi
-
-# set -eux
-
-echo "/usr/bin/zsh ${wiki_path}/scripts/attu_updates.zsh" | at now +2 hours
-
-# Ensure the backup directory exists
-mkdir -p "$backup_path" || {
-    printf '%s\n' "Failed to create backup directory: $backup_path"
-    exit 1
-}
-mkdir -p "$bot_backup_path" || {
-    printf '%s\n' "Failed to create backup directory: $backup_path"
-    exit 1
-}
-
-# Generate filenames with current date
 current_date=$(date +%Y-%m-%d)
-db_backup_file="${backup_path}/attu-wiki-backup_${current_date}.sql.bz2"
 current_month=$(date +%Y-%m)
+
+# attu wiki config
+backup_path='/srv/backups/attu-wiki'
+heartbeat_url="https://uptime.betterstack.com/api/v1/heartbeat/${BACKUPS_HEARTBEAT_KEY}"
+min_backup_size=10000
+wiki_path="$PWD"  # /srv/services/attu-wiki-prod
+mw_container='mediawiki'
+db_container='database'
+
+# doom-bot config
+bot_backup_path='/srv/backups/attu-bot'
+bot_path='/srv/services/doom-bot'
+bot_database_path='assets/markers.db'
+bot_container='core'
+
+mkdir -p "$backup_path" "$bot_backup_path"  # sanity check
+
+# TODO: any way to store a diff of this?
+printf '%s\n' "backup: attu wiki database"
+db_backup_file="${backup_path}/attu-wiki-backup_${current_date}.sql.bz2"
+
+docker compose -f "$wiki_path/docker-compose.yml" \
+    exec "$db_container" mariadb-dump --user=attu --password="$ATTU_DB_PASSWORD" --lock-tables --databases attu_wiki \
+    | bzip2 >"$db_backup_file"
+
+printf '%s\n' "chore: ${mw_container} maintenance tasks"
+docker compose -f "$wiki_path/docker-compose.yml" \
+    exec "$mw_container" php maintenance/run.php cleanupUploadStash
+
+printf '%s\n' "backup: attu images"
 images_backup_file="${backup_path}/attu-images-backup_${current_month}.tar.bz2"
 
-# Backup the database
-printf '%s\n' "Backing up attu wiki database to [$db_backup_file]"
-docker compose -f "$wiki_path/docker-compose.yml" exec "$db_container" mariadb-dump --user=attu --password="$ATTU_DB_PASSWORD" --lock-tables --databases attu_wiki | bzip2 >"$db_backup_file"
+cd "$wiki_path"
+find images -mtime -1 -type f -print0 | tar --null -cjf "$images_backup_file" --no-recursion -T -
 
-# Check if the database backup succeeded
-if [ $? -ne 0 ]; then
-    printf '%s\n' "Database backup failed!"
-    curl -fsS "${heartbeat_url}/fail" >/dev/null
-    exit 1
-fi
-
-# Run maintenance scripts
-printf '%s\n' "Cleaning up MediaWiki upload stash"
-docker compose -f "$wiki_path/docker-compose.yml" exec "$mw_container" php maintenance/run.php cleanupUploadStash
-
-# Backup images daily (update the monthly archive with changes)
-printf '%s\n' "Updating monthly image backup for [$current_month] at [$images_backup_file]"
-cd "$wiki_path" || {
-    printf '%s\n' "Failed to cd to $wiki_path"
-    exit 1
-}
-
-# Find files modified in the last day and update the archive
-find images -mtime -1 -type f -print0 | tar --null -cjf "$images_backup_file" --no-recursion -T - || {
-    printf '%s\n' "Failed to update image backup"
-    exit 1
-}
-
-# Weekly SQLite backup for attu-bot (runs only on Saturdays)
 if [[ $(date +%u) -eq 6 ]]; then
     printf '%s\n' "backup: doom-bot database"
     sqlite_backup_file="$bot_backup_path/markers-$(date +%Y-%-m-%-d).sql"
-    sqlite3 "$doombot_db_path" .dump > "$sqlite_backup_file"
+    docker compose -f "$bot_path/docker-compose.yml" \
+        exec core sqlite3 "$bot_database_path" .dump \
+        > "$sqlite_backup_file"
 fi
 
-# Compact backups on the first day of the month
-day_of_month=$(date '+%d')
+# compact backups (monthly)
+if [ "$(date '+%d')" -eq 1 ]; then
+    printf '%s\n' "bundle: attu wiki database backups"
 
-if [ "$day_of_month" -eq 1 ]; then
     prev_datestamp=$(date -d 'yesterday 13:00' '+%Y-%m')
     compact_prefix="${backup_path}/attu-wiki-backup_${prev_datestamp}"
-    cd "$backup_path" || {
-        printf '%s\n' "Failed to cd to $backup_path"
-        exit 1
-    }
+    cd "$backup_path"
 
-    printf '%s\n' "Compacting backups from previous month to [$compact_prefix.tar.bz2]"
-
-    bunzip2 -v "${compact_prefix}-"*.sql.bz2 || {
-        printf '%s\n' "Failed to unzip .bz2 SQL backups"
-        exit 1
-    }
-
-    tar --remove-files -cjvf "${compact_prefix}.tar.bz2" *.sql || {
-        printf '%s\n' "Failed to create compacted archive"
-        exit 1
-    }
+    bunzip2 -v "${compact_prefix}-"*.sql.bz2
+    tar --remove-files -cjvf "${compact_prefix}.tar.bz2" *.sql
 fi
 
-# # Set ownership of backup files
-# printf '%s\n' "Setting ownership of backup files to jhn:jhn"
-# chown -Rc jhn:jhn "$backup_path" || {
-#     printf '%s\n' "Failed to set ownership"
-#     exit 1
-# }
+# TODO: give this its own timer
+echo "/usr/bin/zsh ${wiki_path}/scripts/attu_updates.zsh" | at now +2 hours
 
-# Verify backup size and send heartbeat
+# verify backup size and send heartbeat
 backup_size=$(wc -c <"$db_backup_file")
 if [ "$backup_size" -ge "$min_backup_size" ]; then
-    printf '%s\n' "Backup succeeded (size: $backup_size bytes). Sending heartbeat."
     curl -fsS "$heartbeat_url" >/dev/null
 else
-    printf '%s\n' "Backup size is too small (size: $backup_size bytes). Sending failure heartbeat."
-    curl -fsS "${heartbeat_url}/fail" >/dev/null
+    printf '%s\n' "failure: wiki backup too small ($backup_size bytes)"
     exit 1
 fi
