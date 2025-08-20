6ce6a177f1c068cf1016add794ed6536cfa818e6
John Hancock <jhnhnck@proton.me>
2025-08-20 14:04:29 -0400

    move venv path

--- scripts/attu_error_rate.zsh
+++ scripts/attu_error_rate.zsh
@@ -3,6 +3,6 @@
 SCRIPT_SOURCE=${0%/*}
 cd $SCRIPT_SOURCE/..
 
-source ./scripts/.venv/bin/activate && python ./scripts/attu_error_rate.py
+source .venv/bin/activate && python ./scripts/attu_error_rate.py
 
 exit
