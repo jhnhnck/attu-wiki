d3fa3a2983af3cb3ca1abd4573113c705eec9644
John Hancock <jhn@xffxe4.lol>
2026-05-31 03:26:05 +0000

    revert(templates): remove forked wikipedia templates, restore refresh list

--- scripts/chores/templates_refresh.zsh
+++ scripts/chores/templates_refresh.zsh
@@ -7,24 +7,23 @@ templates_xml=$(mktemp --suffix=.xml)
 trap 'rm -vf "$templates_xml"' EXIT
 
 # Disabled:'Template:IPA'
-# Moved to wiki/Template/ (managed in repo, synced via load_modules):
-#   Composition bar, Taxobox, Did you mean box, Infobox military unit,
-#   Infobox, MessageBox, Color box, Main, Cquote
-# Add entries here to pull upstream Wikipedia updates for any of the above.
 template_list() {
-    : # nothing to refresh
+    printf '%s%%0A' \
+        'Template:Composition bar' \
+        'Template:Taxobox' \
+        'Template:Did you mean box' \
+        'Template:Infobox military unit' \
+        'Template:Infobox' \
+        'Template:MessageBox' \
+        'Template:Color box' \
+        'Template:Main' \
+        'Template:Cquote'
 }
-
-pages=$(template_list)
-if [[ -z "$pages" ]]; then
-    printf '%s\n' "No templates to refresh"
-    exit 0
-fi
 
 # download templates
 printf '%s\n' "Fetching new templates"
 curl -LsSf 'https://en.wikipedia.org/w/index.php?title=Special:Export' \
-    -d "&pages=${pages}&curonly=1&action=submit&templates=1" \
+    -d "&pages=$(template_list)&curonly=1&action=submit&templates=1" \
     -o "$templates_xml"
 sudo chmod a+r "$templates_xml"
 
